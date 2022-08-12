20f8d036c4f8737d42b67f2574a0d660d8b867cc
klforthwind <kl.forthwind@gmail.com>
2022-08-12 16:29:33 -0500

    add a deposits reader

--- main/csv_file.py
+++ main/csv_file.py
@@ -5,6 +5,7 @@ class CSVFile(File):
 
     def __init__(self, file, perms='w') -> None:
         super().__init__(file, perms)
+        self.deposits = dict()
 
     def process_crash_sites(self, json) -> None:
         """Processes JSON data, adding relevant crash site info to a CSV file."""
@@ -53,4 +54,33 @@ class CSVFile(File):
 
             self._write(f'{item_id},{item_type},{item_count},{concat_coords}\n')
         self.close()
+
+    def process_ores(self, json) -> None:
+        if self._is_closed:
+            print('File is closed - Unable to process :(')
+            return
+        for o in json['objects']:
+            if 'BP_ResourceDeposit' in o['pathName']:
+                coords_lst = o['transform']['translation']
+                concat_coords = ','.join(map(str, coords_lst))
+                obj_props = o['entity']['properties'][0]
+
+                if obj_props['name'] == 'mResourceDepositTableIndex':
+                    key = concat_coords
+                    val = obj_props['value']
+                    if key not in self.deposits:
+                        self.deposits[key] = val
+                    elif self.deposits[key] != val:
+                        self.deposits[key] = 21
+            elif ('Char_Player' in o['pathName'] and
+                    'Char_Player_C' in o['className']):
+                coords_lst = o['transform']['translation']
+                concat_coords = ','.join(map(str, coords_lst))
+                self._write(f'{concat_coords},{44}\n')
+    
+    def finish_up(self) -> None:
+        for k in self.deposits.keys():
+            self._write(f'{k},{str(self.deposits[k])}\n')
+        self.close()
+
 
